7a032ece70be1e1d97b51c1ecf976473aa706984
ivancollazoo <collazoivan1412@gmail.com>
2025-01-29 22:07:58 -0400

    Java is NOT for me code

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -2,8 +2,17 @@ package demo1;
 
 public class DemoTest3 {
 public static void main(String[] args) {
+	
+	int a = 100;
+	int b = 200;
+	
+	if (a > b) {
+		System.out.println("Java is NOT for me");
+	} else {
+		
+	System.out.println("Java");
+	}
 		
 		
-		System.out.println("Java");
 	}
 }
